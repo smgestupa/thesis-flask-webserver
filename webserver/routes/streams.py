002d1cdb7efcc5bf0e1fc232f8bc33ac53ff3cc9
smgestupa <thereal.nwahs@gmail.com>
2024-01-28 00:22:00 +0800

    progress 28/01/24

--- webserver/routes/streams.py
+++ webserver/routes/streams.py
@@ -5,7 +5,7 @@ import requests
 import sqlite3
 import numpy as np
 import cv2
-from threading import Thread, Lock
+from threading import Thread, Event, Lock
 from flask import Blueprint, Response, request, json, make_response, send_file
 from ..extensions import socketio
 from ultralytics import YOLO
@@ -20,6 +20,7 @@ port = os.environ.get('PORT')
 model = YOLO('model/yolov8/yolov8n.onnx', task='detect')
 
 thread = None
+thread_event = Event()
 thread_lock = Lock()
 
 def apply_bounding_boxes(results, image, threshold=0.5):
@@ -70,10 +71,8 @@ def insert_detected_labels(detected_labels, labels_accuracy):
     conn.commit()
     conn.close()
 
-def create_frame(ip_address, image):
-    b64_ip = str(base64.b64encode(ip_address.encode('utf-8')), encoding='utf-8')
-
-    path = f'streams/frames/{b64_ip}'
+def create_frame(b64_ip, image):
+    path = f'videostreams/{b64_ip}'
 
     if not os.path.exists(path):
         os.makedirs(path)
@@ -82,11 +81,12 @@ def create_frame(ip_address, image):
         file.write(image)
 
 def videostreams_get_thread(stream_name, ip_address):
-    socketio.emit(f'{stream_name}_start', "true")
+    socketio.emit(f'{stream_name}_start', 'true')
+    socketio.emit(f'{stream_name}_console', {'message': 'Stream has started.'})
 
     while True:
         start = time.time()
-
+        recording = False
         raw_image = None
         
         retry = 0
@@ -97,7 +97,6 @@ def videostreams_get_thread(stream_name, ip_address):
                 raw_image = None
                 message = f'Could not retrieve image from {ip_address}. Retrying... Number of retries: {retry}'
 
-                print(message)
                 socketio.emit(f'{stream_name}_console', message)
             
             retry += 1
@@ -105,13 +104,20 @@ def videostreams_get_thread(stream_name, ip_address):
         if raw_image == None:
             message = f'Could not retrieve stream from {ip_address}.'
 
-            print(message)
-            socketio.emit(f'{stream_name}_console', message)
-            socketio.emit(f'{stream_name}_stop', "true")
+            socketio.emit(f'{stream_name}_console', {message})
+            socketio.emit(f'{stream_name}_stop', 'true')
 
             thread = None
 
             break
+
+        b64_ip = str(base64.b64encode(ip_address.encode('utf-8')), encoding='utf-8')
+
+        if (os.path.isdir(os.path.join(os.getcwd(), 'videostreams', b64_ip))):
+            recording = True
+            socketio.emit(f'{stream_name}_recording', 'true')
+        else:
+            socketio.emit(f'{stream_name}_recording', 'false')
 
         image_bytes = np.frombuffer(raw_image, dtype=np.uint8)
         image = cv2.imdecode(image_bytes, cv2.IMREAD_COLOR)
@@ -126,7 +132,7 @@ def videostreams_get_thread(stream_name, ip_address):
             _, buffer = cv2.imencode('.jpeg', new_image)
         except:
             message = 'File could not be converted into an image.'
-            socketio.emit(f'{stream_name}_console', message)
+            socketio.emit(f'{stream_name}_console', {message})
             
             thread = None
 
@@ -135,8 +141,8 @@ def videostreams_get_thread(stream_name, ip_address):
         # if total_objs > 0:
         #     Thread(target=insert_detected_labels, args=(detected_labels, labels_accuracy)).start()
 
-        # if request.args.get('record', default='false') == 'true':
-        #     Thread(target=create_frame, args=(camera_id, new_image)).start()
+        if recording:
+            Thread(target=create_frame, args=(b64_ip, new_image)).start()
 
         b64_image = str(base64.b64encode(buffer), encoding='utf-8')
 
@@ -149,21 +155,32 @@ def videostreams_get_thread(stream_name, ip_address):
             'image': b64_image
         })
 
-        print(time.time() - start)
-
         socketio.emit(f'{stream_name}_stream', data)
 
-        socketio.sleep(0.30)
+        socketio.sleep(0.03)
 
 @socketio.on('videostreams_get')
 def handle_videostreams_get(stream_name, ip_address):
     global thread
 
+    thread_event.clear()
+    thread = None
+
+    socketio.emit(f'{stream_name}_console', {'message': f'Trying to connect to the IP address: {ip_address}...'})
+
     with thread_lock:
         if thread is None:
+            thread_event.set()
             thread = socketio.start_background_task(videostreams_get_thread, stream_name, ip_address)
 
-    socketio.emit(
-        f'{stream_name}_console', 
-        {'message': f'Trying to connect to the IP address: {ip_address}...'}
-    )
+@socketio.on('videostreams_record')
+def handle_videostreams_record(ip_address):
+    b64_ip = str(base64.b64encode(ip_address.encode('utf-8')), encoding='utf-8')
+
+    path = f'videostreams/{b64_ip}'
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+        socketio.emit(f'{stream_name}_console', {'message': f'Now recording video stream from {ip_address}.'})
+    else:
+        socketio.emit(f'{stream_name}_console', {'message': f'Video stream from {ip_address} is already being recorded.'})
